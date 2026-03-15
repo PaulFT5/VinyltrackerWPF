c1724fbf31ce25647c5b54d46ffa559b819c3496
PaulFT5 <58445025+PaulFT5@users.noreply.github.com>
2026-03-15 19:32:23 +0200

    Delete Track.cs

--- Track.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VinylTrackerWPF.Models;
-
-public class Track
-{
-    public string Title { get; set; } = string.Empty;
-    public string Duration { get; set; } = string.Empty;
-    public string Side { get; set; } = string.Empty;
-    public int TrackNumber { get; set; }
-}
